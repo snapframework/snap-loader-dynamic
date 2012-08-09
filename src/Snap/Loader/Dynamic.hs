fc1375804aa07498a122c22d62613345d9d900d2
Carl Howells <chowells79@gmail.com>
2012-08-09 09:33:47 -0700

    blacklist the -o option in the interpreter

--- src/Snap/Loader/Dynamic.hs
+++ src/Snap/Loader/Dynamic.hs
@@ -83,7 +83,7 @@ getHintOpts :: [String] -> [String]
 getHintOpts args = removeBad opts
   where
     --------------------------------------------------------------------------
-    bad       = ["-threaded", "-O", "-main-is"]
+    bad       = ["-threaded", "-O", "-main-is", "-o"]
 
     --------------------------------------------------------------------------
     removeBad = filter (\x -> not $ any (`isPrefixOf` x) bad)
