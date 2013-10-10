cb6ab9959e77165d67a1e77f7c011bf8ce29f5f5
Doug Beardsley <mightybyte@gmail.com>
2013-10-10 13:59:30 -0400

    Fix problem with -static

--- src/Snap/Loader/Dynamic.hs
+++ src/Snap/Loader/Dynamic.hs
@@ -83,7 +83,7 @@ getHintOpts :: [String] -> [String]
 getHintOpts args = removeBad opts
   where
     --------------------------------------------------------------------------
-    bad       = ["-threaded", "-O", "-main-is", "-o", "--make"]
+    bad       = ["-threaded", "-O", "-main-is", "-o", "--make", "-static"]
 
     --------------------------------------------------------------------------
     removeBad = filter (\x -> not $ any (`isPrefixOf` x) bad)
